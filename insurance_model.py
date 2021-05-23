8864dd49e01d08ba92280839b6ca9374c96af28c
sf11-hub <sefigas@gmail.com>
2021-05-23 11:53:00 +0200

    Git-1

--- insurance_model.py
+++ insurance_model.py
@@ -1,14 +1,15 @@
-from pycaret.regression import *
+from pycaret.regression import setup, create_model, tune_model, save_model
+import pandas as pd
 
-data = pd.read_csv('C:/tmp/insurance.csv',  delimiter = ',')
+data = pd.read_csv('C:/tmp/insurance.csv',  delimiter=',')
 print(data.head())
 
-r2 = setup(data, target = 'charges', session_id = 123,
-           normalize = True,
-           polynomial_features = True, trigonometry_features = True,
+r2 = setup(data, target='charges', session_id=123,
+           normalize=True,
+           polynomial_features=True, trigonometry_features=True,
            feature_interaction=True,
-           bin_numeric_features= ['age', 'bmi'])
+           bin_numeric_features=['age', 'bmi'])
 
 lr = create_model('lr')
 tuned_lr = tune_model(lr)
-save_model(tuned_lr, model_name = 'lr_deployment_20210521')
+save_model(tuned_lr, model_name='lr_deployment_20210521')
---
+++ test_ML_streamlit_app.py
@@ -0,0 +1,12 @@
+import ML_streamlit_app as app
+from pycaret.regression import load_model
+import pandas as pd
+
+model = load_model('lr_deployment_20210521')
+input_dict = {'age': 30, 'sex': 'M', 'bmi': 15,
+              'children': 1, 'smoker': 'yes', 'region': 'nortwest'}
+input_df = pd.DataFrame([input_dict])
+
+class TestMLapp:
+    def test_predict(self):
+        assert 1000 < app.predict(model=model, data=input_df)
