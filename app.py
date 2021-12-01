174dc1d3018539d8c1a29bc3b6d605ef72ad5c2b
sf11-hub <52853413+sf11-hub@users.noreply.github.com>
2021-12-01 23:30:21 +0100

    Update app.py
    B

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ import uvicorn
 # 2. Create the app object
 app = FastAPI()
 
-#. Load trained Pipeline
+# Load trained Pipeline
 model = load_model('./models/lr_deployment_20210521')
+
 
 # Define predict function
 @app.post('/predict')
@@ -18,6 +19,7 @@ def predict(age, sex, bmi, children, smoker, region):
 
     predictions = predict_model(model, data=data)
     return {'prediction': int(predictions['Label'][0])}
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
